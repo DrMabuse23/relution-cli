bf24c41691ee99cc6260606d45f4ddb6f27d76b5
DrMabuse23 <p.brewing@mwaysolutions.com>
2015-09-22 09:46:19 +0200

    add header

--- src/commands/deploy-command.js
+++ src/commands/deploy-command.js
@@ -19,30 +19,3 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-
-require("babel/polyfill");
-import Tower from './commands/tower';
-import StartTree from './components/StartTree';
-import RelutionCli from './helper/relution';
-
-export default class Cli{
-  constructor(opts){
-    let self = this;
-    this.helper = new RelutionCli();
-    this.tower = new Tower();
-    this.opts = opts.argv;
-    this.commands = null;
-    this.run();
-  }
-
-  run(){
-    var self = this;
-    var start = new StartTree();
-    return this.helper.getProjectType().then(() => {
-      self.commands = self.tower.getCommandsByType(self.helper.projectType);
-      var flat = self.tower.getStartCommands(self.commands);
-      return self.tower.showCommands('Start', 'Please choose Your Command', flat);
-    });
-  }
-
-}
